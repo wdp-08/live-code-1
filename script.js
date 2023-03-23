fbd1be87af240882145da672c95aa6953c7a3604
Candra-Dv <Candradermawansyah.com>
2023-03-23 21:05:58 +0700

    menambahkan js

---
+++ script.js
@@ -0,0 +1,13 @@
+const kata = "Ini Buah-buahan";
+console.log(kata);
+
+let fruit = ["pisang", "pepaya", "durian", "melon", "semangka"]
+
+fruit.push('mangga');
+console.log(fruit);
+
+fruit[3] = "strawberi"
+console.log(fruit);
+
+fruit.pop();
+console.log(fruit);
