02622668be015c67ae7a943ae786536966105c8f
silviansi <silviansi24@gmail.com>
2023-03-23 20:45:13 +0700

    update js

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ let fruit = ["Durian", "Apel", "Anggur", "Kurma", "Lemon"];
 
 //Menambah satu buah
 fruits.push("Melon");
+fruit.push("Melon");
 console.log(fruits, fruit);
 
 //Kurangi satu buah
-
+fruits.pop();
+fruit.pop();
+console.log(fruits, fruit);
 
 //Mengubah satu buah
 fruits[2] = "Semangka";
+fruit[2] = "Semangka";
 console.log(fruits, fruit);
