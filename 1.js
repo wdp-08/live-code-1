d01c16f77bd3ed2c79f7d5e28736e075c0aa53a1
Dodistywn <dodisetya000@gmail.com>
2023-03-23 20:45:15 +0700

    membuat file 1.js dan index.html

---
+++ 1.js
@@ -0,0 +1,11 @@
+const buah = ["Apel", "Jeruk", "Manggis", "Nanas", "Anggur"];
+console.log(buah);
+
+buah.push("Duren");
+console.log(buah);
+
+buah[2] = "Mangga";
+console.log(buah);
+
+buah.pop()
+console.log(buah);
