befaf2eb24329f6738ddee90ef40ef0c7ec33edd
Delin Herliana Pashs <delinherlianapasha@gmail.com>
2023-03-23 22:53:50 +0700

    live1

---
+++ script.js
@@ -0,0 +1,16 @@
+let nama;
+nama = "delin";
+console.log(nama);
+
+// const
+const binatang = "kucing";
+console.log(binatang);
+
+//array
+let buah = ["mangga", "jeruk", "anggur", "apel", "pisang"];
+buah.push("tomat");
+console.log(buah);
+
+buah.splice(4);
+buah[1] = "jeruk";
+console.log(buah);
