ab138607c0ae4a27801bb573992cf1f154a1c5b9
Dwik <dwikja1@gmail.com>
2023-03-23 20:33:47 +0700

    live code 1

---
+++ livecode1.js
@@ -0,0 +1,12 @@
+let a = "Hallo, Selamat Malam";
+console.log(a);
+const b = 25;
+console.log(b);
+
+let fruit = ["stroberi", "mangga", "apel", "durian", "alpukat"];
+
+fruit.push("kurma");
+fruit[3] = "lengkeng";
+fruit.pop();
+
+console.log(fruit);
