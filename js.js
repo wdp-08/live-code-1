7c3df9fc49fdb1c4f2c080009bc9578dd2bbe58f
AdiPutra <madiputra0107@gmail.comgit config --global user.name madiputra0107@gmail.com>
2023-03-23 21:01:51 +0700

    baru

---
+++ js.js
@@ -0,0 +1,9 @@
+let a = "varibel";
+const b = "constanta";
+console.log(a);
+console.log(b);
+a = "new variable";
+console.log(a);
+
+let fruits = ["apple", "pisang", "anggur", "jeruk", "nanas"];
+console.log(fruits);
