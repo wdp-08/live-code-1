ab613851d636b3bb289ac3acd7cdc6da8e6730e5
Rivaldi Alwi <rivaldialwi42@gmail.com>
2023-03-23 20:32:07 +0700

    Update Js

---
+++ main.js
@@ -0,0 +1,14 @@
+const hewan = ["ayam", "kucing", "ikan"]
+let buah = ["apel", "melon", "semangka"]
+
+// console.log(hewan);
+// console.log(buah);
+
+// buah.push("pisang")
+// console.log(buah);
+
+// buah[0] = "pisang" 
+// console.log(buah);
+
+// buah.pop()
+// console.log(buah);
