d266127ecbf64ad191045d5c2f37cd881884b8a6
unknown <kevinlukas1803@gmail.com>
2023-03-23 20:45:03 +0700

    create js.js

---
+++ js.js
@@ -0,0 +1,10 @@
+let let a = "variable";
+const b = "constant";
+console.log(a);
+console.log(b);
+a = "new variable";
+console.log(a);
+
+
+let fruits = ["mangga", "jeruk", "pisang", "anggur", "melon"];
+console.log(fruits);
