c929eda32e7470853236300a8cb5bf3fd650b050
Rifqiyah162001 <rifqiyahramadani62@gmail.com>
2023-03-23 20:31:30 +0700

    live-code-1

---
+++ script.js
@@ -0,0 +1,7 @@
+const fruits = ["Apple", "Orange", "Watermelon", "Mango", "Grape"];
+console.log(fruits);
+fruits.push("Salak");//tambah
+console.log(fruits);
+fruits.splice(2,1,"Melon")//edit 
+console.log(fruits);
+fruits.pop("Salak");//hapus
