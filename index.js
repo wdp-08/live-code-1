eccf40ebbf2505ef7a5cefa68a134687116964ea
Kevin <vnctkevin@gmail.com>
2023-03-23 20:33:27 +0700

    WIP on the assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+let a = "variable";
+const b = "constant";
+console.log(a);
+console.log(b);
+a = "new variable";
+console.log(a);
+// b = "new constant"; // TypeError: Assignment to constant variable.
+
+let fruits = ["apple", "banana", "orange", "mango", "grapes"];
+console.log(fruits);
