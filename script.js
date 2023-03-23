d3e05106bad7f10bb8ff0862eab938941290d01e
silviansi <silviansi24@gmail.com>
2023-03-23 20:38:34 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//Membuat array berisi 5 buah-buahan
+const fruits = ["Apel", "Belimbing", "Chery", "Durian", "Jambu"];
+let fruit = ["Durian", "Apel", "Anggur", "Kurma", "Lemon"];
+
+//Menambah satu buah
+fruits.push("Melon");
+console.log(fruits, fruit);
+
+//Kurangi satu buah
+
+
+//Mengubah satu buah
+fruits[2] = "Semangka";
+console.log(fruits, fruit);
