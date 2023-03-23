b78b3d881b2b1d4c28a5554160f2b3d365acfa47
Triyonorifan11 <triyonorifan01@gmail.com>
2023-03-23 20:44:58 +0700

    update for delete and edit

--- index.js
+++ index.js
@@ -14,10 +14,21 @@ create_new_buah('pisang', dataBuah)
 function create_new_buah(data, array){
     return array.push(data)
 }
-console.log(dataBuah)
+console.log('add', dataBuah)
 // edit
 function edit_buah(index, array, data){
     for(let i = 0; i<array.length; i++){
         array[index] = data;
     }
 }
+edit_buah(2, dataBuah, 'Jeruk Macan')
+console.log('Edit', dataBuah)
+
+
+// hapus
+function delete_buah(array){
+    return array.pop()
+}
+
+delete_buah(dataBuah)
+console.log('Delete', dataBuah)
