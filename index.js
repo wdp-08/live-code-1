ce86fbd89cada505ecec940de3abe34dd5971e66
Kevin <vnctkevin@gmail.com>
2023-03-23 21:09:00 +0700

    Finish Live Code 1

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ console.log(a);
 
 let fruits = ["apple", "banana", "orange", "mango", "grapes"];
 console.log(fruits);
+// tambahkan 1 buah
+fruits.push("strawberry");
+console.log(fruits);
+// edit 1 buah
+fruits[0] = "pineapple";
+console.log(fruits);
+// hapus 1 buah terakhir
+fruits.pop();
+console.log(fruits);
