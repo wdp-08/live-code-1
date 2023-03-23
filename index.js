a59262cfc3d0292b96666f63211d1adfc090a83a
ararizqia <ararizqiaswin@gmail.com>
2023-03-23 20:54:50 +0700

    menambahkan nama buah baru

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+// membuat array
 let fruits = ['nangka', 'salak', 'pisang', 'pepaya', 'jeruk'];
+console.log(fruits);
+
+//menambahkan nama buah
+let fruit = fruits.push('rambutan');
 console.log(fruits);
