7b56f8738f48ae2bfec759390d769a4fe26298a5
SalmO-On <fitrianurul0213@gmail.com>
2023-03-23 21:08:39 +0700

    hasil live-code-1 fitria tambahan

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const buah = ["apel", "bluberry", "anggur", "durian", "semangka"];
-buah.push("salak");
+buah.push("salak");//tambah buah
+buah[2] = 'duku';//ganti edit
+buah.pop();//hapus terakhir
 for (let i = 0; i < buah.length; i++) {
     console.log(buah[i]);
 }
